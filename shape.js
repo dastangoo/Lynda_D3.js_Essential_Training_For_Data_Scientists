8ffeb33f3c9016ebc2fd2f4b78e373f82cdd8018
dastangoo <hamed.dastangoo@gmail.com>
2018-01-01 22:52:08 +0330

    Adding an SVG

--- shape.js
+++ shape.js
@@ -1,1 +1,7 @@
-console.log("Hello");
+var dataArray = [5, 11, 18];
+
+// With jquery, you can manipulate DOM
+// $("body").append("<svg></svg>");
+
+// With d3.js, you can manipulate DOM
+d3.select("body").append("svg").attr("height", "100%").attr("width", "100%");
