e025512373d7bd75055a095b343b858cdaffe27d
dastangoo <hamed.dastangoo@gmail.com>
2018-01-01 23:15:39 +0330

    Setting attributes for shapes

--- shape.js
+++ shape.js
@@ -5,4 +5,9 @@ var dataArray = [5, 11, 18];
 
 // With d3.js, you can manipulate DOM
 var svg = d3.select("body").append("svg").attr("height", "100%").attr("width", "100%");
-svg.selectAll("rect").data(dataArray).enter().append("rect");
+svg.selectAll("rect").data(dataArray).enter().append("rect")
+      .attr("height", function (d, i) { return d*15; } )
+      .attr("width", "50")
+      .attr("fill", "pink")
+      .attr("x", function (d, i) { return 60*i; } )
+      .attr("y", function (d, i) { return 300-(d*15); });
