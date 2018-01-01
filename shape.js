d76a476eb879085d74a5467a7c1a3f8cae848c8e
dastangoo <hamed.dastangoo@gmail.com>
2018-01-01 23:26:21 +0330

    Seeing the code how the browser sees it

--- shape.js
+++ shape.js
@@ -6,8 +6,8 @@ var dataArray = [5, 11, 18];
 // With d3.js, you can manipulate DOM
 var svg = d3.select("body").append("svg").attr("height", "100%").attr("width", "100%");
 svg.selectAll("rect").data(dataArray).enter().append("rect")
-      .attr("height", function (d, i) { return d*15; } )
+      .attr("height", function (d) { return d*15; } )
       .attr("width", "50")
       .attr("fill", "pink")
-      .attr("x", function (d, i) { return 60*i; } )
-      .attr("y", function (d, i) { return 300-(d*15); });
+      .attr("x", function (d, i) { return i * 60; } )
+      .attr("y", function (d, i) { return 300 - (d * 15); });
