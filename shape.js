9ee6c222e9b5aeb8d69b9918274e16755dbae5c0
dastangoo <hamed.dastangoo@gmail.com>
2018-01-02 19:41:58 +0330

    Drawing ellipses

--- shape.js
+++ shape.js
@@ -6,19 +6,31 @@ var dataArray = [5, 11, 18];
 // With d3.js, you can manipulate DOM
 var svg = d3.select("body").append("svg").attr("height", "100%").attr("width", "100%");
 svg.selectAll("rect").data(dataArray).enter().append("rect")
-      .attr("height", function (d) { return d*15; } )
+      .attr("height", function (d) { return d * 15 } )
       .attr("width", "50")
       .attr("fill", "pink")
       .attr("x", function (d, i) { return i * 60; } )
       .attr("y", function (d, i) { return 300 - (d * 15); });
 
 var newX = 300;
-svg.selectAll("circle").data(dataArray).enter().append("circle")
+svg.selectAll("circle.first").data(dataArray).enter().append("circle")
+      .attr('class', 'first')
       .attr('cx', function (d, i) {
-        newX += (d * 6) + (i * 20);
+        newX += (d * 3) + (i * 20);
         return newX;
       })
       .attr('cy', '90')
       .attr('r', function (d) {
-        return d * 3;
-      });
+        return d * 3; });
+        
+var newX = 600;
+svg.selectAll("ellipse").data(dataArray).enter().append("ellipse")
+      .attr('cx', function (d, i) {
+        newX += (d * 3) + (i * 20);
+        return newX;
+      })
+      .attr('cy', '90')
+      .attr('rx', function (d) {
+        return d * 3; })
+      .attr('ry', '30');
+        
