c95f1e2b65415fa001d3bff322cac503682e50f5
dastangoo <hamed.dastangoo@gmail.com>
2018-01-06 20:00:08 +0330

    Creating a linear axis

--- area.js
+++ area.js
@@ -1,18 +1,18 @@
-var dataArray = [25, 26, 28, 32, 37, 45, 55, 70, 90, 120, 135, 150, 160, 172, 177, 180];
-var dataYears = ['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016'];
+var dataArray = [25, 26, 28, 32, 37, 45, 55, 70, 90, 120, 135, 150, 160, 172, 177, 180],
+    dataYears = ['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016'], height, width, y, area, svg, yAxis, margin, chartGroup;
 
-var height = 200;
-var width = 500;
+height = 200;
+width = 500;
 
-var y = d3.scaleLinear()
+margin = {left: 50, right: 50, top: 40, bottom: 0};
+y = d3.scaleLinear()
             .domain([0, 180])
             .range([height, 0]);
 
 
-console.log(y(0));
-console.log(y(90));
-console.log(y(180));
-var area = d3.area()
+yAxis = d3.axisLeft(y);
+
+area = d3.area()
                 .x(function (d, i) {
                   return i * 20;
                 })
@@ -21,5 +21,7 @@ var area = d3.area()
                   return y(d);
                 });
 
-var svg = d3.select("body").append("svg").attr("height", "100%").attr("width", "100%");
-svg.append("path").attr("d", area(dataArray));
+svg = d3.select("body").append("svg").attr("height", "100%").attr("width", "100%");
+chartGroup = svg.append("g").attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+chartGroup.append("path").attr("d", area(dataArray));
+chartGroup.append("g").attr("class", "axis y").call(yAxis);
