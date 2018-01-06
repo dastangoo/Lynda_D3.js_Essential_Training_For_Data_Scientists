d78af4d70f2c76e4df76a994ef4bef01f860ecb0
dastangoo <hamed.dastangoo@gmail.com>
2018-01-06 18:28:26 +0330

    Adding text

--- shape.js
+++ shape.js
@@ -4,7 +4,7 @@ var dataArray = [5, 11, 18];
 // $("body").append("<svg></svg>");
 
 // With d3.js, you can manipulate DOM
-var svg = d3.select("body").append("svg").attr("height", "100%").attr("width", "100%");
+var svg = d3.select("body").append("svg").attr("height", "300").attr("width", "100%");
 svg.selectAll("rect").data(dataArray).enter().append("rect")
       .attr("height", function (d) { return d * 15 } )
       .attr("width", "50")
@@ -49,3 +49,46 @@ svg.selectAll("line").data(dataArray).enter().append("line")
       .attr('y2', function (d, i) {
         return 80 + (i * 20);
       });
+var textArray = ['start', 'middle', 'end'];
+svg.append('text').selectAll('tspan')
+  .data(textArray)
+  .enter().append('tspan')
+  .attr('x', newX)
+  .attr('y', function (d, i) {
+    return 150 + (i * 30);
+  })
+  .attr('fill', 'none')
+  .attr('stroke', 'blue')
+  .attr('stroke-width', '2')
+  .attr('font-size', '30')
+  .attr('text-anchor', 'start')
+  .attr('dominant-baseline', 'middle')
+  .text(function (d) {
+    return d;
+  });
+  
+// svg.append('text')
+//   .attr('x', newX)
+//   .attr('y', 180)
+//   .attr('fill', 'blue')
+//   .attr('stroke', 'none')
+//   .attr('font-size', '30')
+//   .attr('text-anchor', 'middle')
+//   .attr('dominant-baseline', 'middle')
+//   .text('Middle');
+// 
+// svg.append('text')
+//   .attr('x', newX)
+//   .attr('y', 210)
+//   .attr('stroke', 'blue')
+//   .attr('fill', 'none')
+//   .attr('font-size', '30')
+//   .attr('text-anchor', 'end')
+//   .attr('dominant-baseline', 'middle')
+//   .text('End');
+  
+svg.append("line")
+  .attr('x1', newX)
+  .attr('y1', '150')
+  .attr('x2', newX)
+  .attr('y2', '210');
